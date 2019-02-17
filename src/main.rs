699cce080ea4bee98997da2ce39f3d3e6f0203b3
Nick Murphy <comfortablynick@gmail.com>
2019-02-17 13:21:23 -0600

    Add fmt string iterator

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,11 @@ use std::path::PathBuf;
 use std::process::{Command, Output};
 use std::{env, str};
 
+// struct Options {
+//
+// }
+
 #[derive(Debug)]
-#[allow(dead_code)]
 struct Repo {
     working_dir: Option<PathBuf>,
     git_dir: Option<String>,
@@ -21,8 +24,17 @@ struct Repo {
     unmerged: u32,
     insertions: u32,
     deletions: u32,
-    // Unstaged   GitArea
-    // Staged     GitArea
+    unstaged: GitArea,
+    staged: GitArea,
+}
+
+#[derive(Debug)]
+struct GitArea {
+    modified: u32,
+    added: u32,
+    deleted: u32,
+    renamed: u32,
+    copied: u32,
 }
 
 impl Repo {
@@ -41,6 +53,20 @@ impl Repo {
             unmerged: 0,
             insertions: 0,
             deletions: 0,
+            unstaged: GitArea {
+                modified: 0,
+                added: 0,
+                deleted: 0,
+                renamed: 0,
+                copied: 0,
+            },
+            staged: GitArea {
+                modified: 0,
+                added: 0,
+                deleted: 0,
+                renamed: 0,
+                copied: 0,
+            },
         }
     }
     /* Parse git status by line */
@@ -66,11 +92,9 @@ impl Repo {
                         }
                     }
                 }
-                if word == "1" {
-                    trace!("Tracked file: {}", line);
-                }
-                if word == "2" {
-                    trace!("Renamed file: {}", line);
+                // Tracked file
+                if word == "1" || word == "2" {
+                    self.parse_modified(words.next().unwrap());
                 }
                 if word == "u" {
                     trace!("Unmerged file: {}", line);
@@ -79,6 +103,25 @@ impl Repo {
                     trace!("Untracked file: {}", line);
                 }
             }
+        }
+    }
+
+    fn parse_modified(&mut self, ln: &str) -> () {
+        match &ln[..1] {
+            "M" => self.staged.modified += 1,
+            "A" => self.staged.added += 1,
+            "D" => self.staged.deleted += 1,
+            "R" => self.staged.renamed += 1,
+            "C" => self.staged.copied += 1,
+            _ => (),
+        }
+        match &ln[1..] {
+            "M" => self.unstaged.modified += 1,
+            "A" => self.unstaged.added += 1,
+            "D" => self.unstaged.deleted += 1,
+            "R" => self.unstaged.renamed += 1,
+            "C" => self.unstaged.copied += 1,
+            _ => (),
         }
     }
 }
@@ -104,9 +147,37 @@ fn main() {
     std::env::set_var("RUST_LOG", "trace");
     std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
+    const FMT_STRING: &str = "%g (%b) %a %% %m%d%u%t %s";
+    trace!("format str: {}", &FMT_STRING);
     let cmd = run("git", &["status", "--porcelain=2", "--branch"]);
     let status = str::from_utf8(&cmd.stdout).unwrap();
     let mut ri = Repo::new();
     ri.parse_status(&status);
-    println!("{:#?}", ri);
+
+    // parse fmt string
+    let mut fmt_str = FMT_STRING.chars();
+    let mut out: String = String::new();
+    while let Some(c) = fmt_str.next() {
+        if c == '%' {
+            if let Some(c) = fmt_str.next() {
+                match &c {
+                    ' ' => out.push(' '),
+                    'a' => out.push('a'),
+                    'b' => out.push_str(&ri.branch.as_ref().unwrap().to_string()),
+                    'c' => trace!("show commit"),
+                    'd' => trace!("show diff"),
+                    'g' => trace!("show br glyph"),
+                    'm' => trace!("show modified"),
+                    'n' => trace!("show VCS name"),
+                    's' => trace!("show stage modified"),
+                    '%' => out.push('%'),
+                    _ => (),
+                }
+            }
+        } else {
+            out.push(c);
+        }
+    }
+    info!("{:#?}", ri);
+    info!("Output: {}", &out);
 }
